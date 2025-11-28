b6a1eb1caac6d6c51fc656f647df9fcfe22413da
FelixPhu <phufelix11@gmail.com>
2025-11-27 22:44:41 -0500

    added new files

---
+++ src/main/java/org/sciborgs1155/robot/drive/Drive.java
@@ -0,0 +1,17 @@
+package org.sciborgs1155.robot.drive;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.sciborgs1155.robot.Ports;
+
+import com.revrobotics.CANSparkBase.IdleMode;
+
+public class Drive extends SubsystemBase {
+    private final CANSparkMax leftLeader = new CANSparkMax(Ports.Drive.LEFT_LEADER, MotorType.kBrushless);
+    private final CANSparkMax leftFollower = new CANSparkMax(Ports.Drive.LEFT_FOLLOWER, MotorType.kBrushless);
+    private final CANSparkMax rightLeader = new CANSparkMax(Ports.Drive.RIGHT_LEADER, MotorType.kBrushless);
+    private final CANSparkMax rightFollower = new CANSparkMax(Ports.Drive.RIGHT_FOLLOWER, MotorType.kBrushless);
+}
+
